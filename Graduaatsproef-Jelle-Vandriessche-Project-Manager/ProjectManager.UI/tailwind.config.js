0653e2732d1ee435c4916cca1a81dd494fc58cba
jellev00 <jelle.vandriessche@gmail.com>
2023-10-15 22:17:24 +0200

    Graduaatsproef

---
+++ Graduaatsproef-Jelle-Vandriessche-Project-Manager/ProjectManager.UI/tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/App.vue',
+    './src/pages/Home.vue',
+    './src/pages/About.vue',
+  ],
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
+
